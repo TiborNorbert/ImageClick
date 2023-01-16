4e75881922887f58a287954934b9a629fe4fcee8
cisco <cisco@B101-01>
2023-01-16 16:18:35 +0100

    Add project files.

---
+++ ImageClick/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace ImageClick
+{
+    partial class ImageStorage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.imagestorgae = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // imagestorgae
+            // 
+            this.imagestorgae.Dock = System.Windows.Forms.DockStyle.Right;
+            this.imagestorgae.Location = new System.Drawing.Point(187, 0);
+            this.imagestorgae.Name = "imagestorgae";
+            this.imagestorgae.Size = new System.Drawing.Size(613, 450);
+            this.imagestorgae.TabIndex = 0;
+            // 
+            // ImageStorage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.imagestorgae);
+            this.Name = "ImageStorage";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel imagestorgae;
+    }
+}
+
---
+++ ImageClick/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageClick
+{
+    public partial class ImageStorage : Form
+    {
+        int Sorokszama = 2;
+        int Oszlopokszama = 2;
+        int ImageSize = 280;
+        public ImageStorage()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < Sorokszama; i++)
+            {
+                for (int j = 0; j < Oszlopokszama; j++)
+                {
+                    PictureBox picture= new PictureBox();
+                    picture.SetBounds(j*ImageSize,i*ImageSize,ImageSize,ImageSize);
+                    picture.Image = Image.FromFile("C:\\Users\\cisco\\Pictures\\monkey.jfif");
+                    picture.Click += (o, ev) =>
+                        {
+                            PictureBox gomb = (PictureBox)o;
+                            gomb.BackgroundImage= picture.Image;
+                        };
+                    imagestorgae.Controls.Add(picture);
+                }
+            }
+        }
+    }
+}
